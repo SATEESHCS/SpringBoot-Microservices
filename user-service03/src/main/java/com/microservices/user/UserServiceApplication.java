bf51e825e8acb988183666e5b1b2ec7b0f9475e8
Sateesh <sateeshraghava1986@gmail.com>
2021-08-22 22:36:55 +0530

    user-service commit

---
+++ user-service03/src/main/java/com/microservices/user/UserServiceApplication.java
@@ -0,0 +1,27 @@
+package com.microservices.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate()
+	{
+		return new RestTemplate();
+	}
+}
